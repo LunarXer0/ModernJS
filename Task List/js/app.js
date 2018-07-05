578012ca786d1ed7430fc0577e90636cf43d4ba7
Victor Rivera <verm2405@gmail.com>
2018-07-04 20:40:19 -0400

    Task List

---
+++ Task List/js/app.js
@@ -0,0 +1,159 @@
+//* Define UI variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//* Load all event listeners
+loadAllEventListeners();
+
+//* Load all event listeners
+function loadAllEventListeners(){
+    //* DOM Load event
+    document.addEventListener('DOMContentLoaded', getTasks);
+
+    // * Add task event 
+    form.addEventListener('submit', addTask);
+
+    //* Remove task event
+    taskList.addEventListener('click', removeTask);
+
+    //* Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+
+    //* Filter tasks
+    filter.addEventListener('keyup', filterTasks);
+}
+
+//* Get Tasks from Local Storage
+function getTasks(){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task){
+        let li = document.createElement('li');
+        li.className = ('collection-item');
+        li.appendChild(document.createTextNode(task));
+        let link = document.createElement('a');
+        link.className = "delete-item secondary-content";
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        li.appendChild(link);
+        taskList.appendChild(li);
+    });
+}
+
+
+//* Add Task
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Add Task');
+    }
+
+    //* Create li element
+    const li = document.createElement('li');
+    
+    //* Add class
+    li.className = 'collection-item';
+    
+    //* Create text node and append
+    li.appendChild(document.createTextNode(taskInput.value));
+    
+    //* Create new link element
+    const link = document.createElement('a');
+    
+    //* Add class
+    link.className = 'delete-item secondary-content';
+    
+    //* Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    
+    //* Append link to li
+    li.appendChild(link);
+
+    //* Append li to UL
+    taskList.appendChild(li);
+
+    //* Store in local storage
+    storeTaskInLocalStorage(taskInput.value);
+
+    //* Clear input 
+    taskInput.value = '';
+
+    e.preventDefault();
+}
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure?')){
+         e.target.parentElement.parentElement.remove();
+
+        //* Remove from Local Storage
+        removeFromLocalStorage(e.target.parentElement.parentElement); 
+        }
+    }
+}
+
+function clearTasks(){
+    //taskList.innerHTML = '';
+
+    //* Faster
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+
+    //* Clear from local Storage
+    clearFromLocalStorage();
+}
+
+function clearFromLocalStorage(){
+    localStorage.clear();
+}
+
+
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+    
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        }
+        else{
+            task.style.display = 'none';
+        }
+    });
+}
+
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function removeFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice (index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
