77795333fc675784cd71b1145113e3c434ccea6a
Victor Rivera <verm2405@gmail.com>
2018-07-13 20:01:42 -0400

    Async & Await Fetch API library

---
+++ EasyHTTP Async Await/easyhttp3.js
@@ -0,0 +1,47 @@
+class EasyHTTP{
+    //* HTTP GET Request
+    async get(url){
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
+
+    //* POST Request
+    async post(url, data){
+           const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            const resData = await response.json();
+            return resData;
+    }
+
+    //* Put Request
+    async put(url, data){
+        
+           const response = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            const resData = await response.json();
+            return resData;
+    }
+
+    //* Delete Request
+    async delete(url){
+            const response = await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+            const resData = await 'Resource Deleted';
+            return resData;
+    }
+}
